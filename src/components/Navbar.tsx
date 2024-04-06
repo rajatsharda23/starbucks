1fddbd9e392ee829d995f27d3df5b145d11b0358
Rajat Sharda <rajatsharda@Rajats-MacBook-Air.local>
2024-04-07 01:37:43 +0530

    added colours

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            upper
+        </div>
+        <div>
+            lower
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      'dark-green' : '#1e3932',
+      'green' : '#008248',
+      'light-green' : '#d4e9e2',
+      'lighter-green' : '#f1f8f6',
+      'mud' : '#f3f1e7',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
